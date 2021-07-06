38f9bcfd93461fe4deecb36d2118226ff560e308
brad medeiros <bradmedeiros0@gmail.com>
2021-07-06 15:32:21 -0700

    fbx exporting -> apply 0.01 scaling factor to fix scaling issue with fbx units

--- export-model.py
+++ export-model.py
@@ -5,12 +5,17 @@ import os
 
 build_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "build")
 
-axis_forward='-Z'
-axis_up='Y'
-
 model_to_build = sys.argv[6]
 output_file = os.path.join(build_dir, model_to_build)
 output_folder = os.path.dirname(output_file)
 
 os.makedirs(output_folder, exist_ok=True)
-bpy.ops.export_scene.fbx(filepath=output_file, axis_forward=axis_forward, axis_up=axis_up)
+bpy.ops.export_scene.fbx(
+  filepath=output_file, 
+  axis_forward='-Z', 
+  axis_up='Y', 
+  apply_unit_scale=True,  
+  apply_scale_options='FBX_SCALE_NONE',
+  global_scale=0.01   # see https://developer.blender.org/T70161
+)
+
