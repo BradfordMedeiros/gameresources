adda16d676d83cfafad3b53526f2878f84f1288b
brad medeiros <bradmedeiros0@gmail.com>
2021-07-20 17:09:05 -0700

    dont rebuild built files, fbx and dae support

--- Makefile
+++ Makefile
@@ -1,10 +1,17 @@
 SOURCES := $(wildcard ./**/*.blend)
-OBJ :=$(patsubst %.blend, %.fbx, ${SOURCES})
 
-all: $(OBJ)
+all: fbx
+	
+dae: $(patsubst ./%.blend, ./build/%.dae, ${SOURCES})
+fbx: $(patsubst ./%.blend, ./build/%.fbx, ${SOURCES})
 
-%.fbx: %.blend
-	@blender $< --background --python ./export-model.py -- $@
+./build/%.fbx: %.blend
+	@echo "Build $@ from $<"
+	@blender $< --background --python ./export-model.py -- $@ fbx > /dev/null
+
+./build/%.dae: %.blend
+	@echo "Build $@ from $<"
+	@blender $< --background --python ./export-model.py -- $@ dae > /dev/null
 
 clean:
 	@rm -rf ./build
--- export-model.py
+++ export-model.py
@@ -3,19 +3,51 @@ import bpy
 import sys
 import os 
 
-build_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "build")
+output_file = sys.argv[6]
+output_format = sys.argv[7]
 
-model_to_build = sys.argv[6]
-output_file = os.path.join(build_dir, model_to_build)
 output_folder = os.path.dirname(output_file)
 
 os.makedirs(output_folder, exist_ok=True)
-bpy.ops.export_scene.fbx(
-  filepath=output_file, 
-  axis_forward='-Z', 
-  axis_up='Y', 
-  apply_unit_scale=True,  
-  apply_scale_options='FBX_SCALE_NONE',
-  global_scale=0.01   # see https://developer.blender.org/T70161
-)
+
+print("output format is: " + output_format)
+
+export_fbx = True
+
+def build_fbx():
+  bpy.ops.export_scene.fbx(
+    filepath=output_file, 
+    axis_forward='-Z', 
+    axis_up='Y', 
+    apply_unit_scale=True,  
+    apply_scale_options='FBX_SCALE_NONE',
+    global_scale=0.01,   # see https://developer.blender.org/T70161
+    add_leaf_bones=False
+  )
+
+def build_dae():
+  bpy.ops.wm.collada_export(
+    filepath=output_file, 
+    export_global_forward_selection='-Z',
+    export_global_up_selection='Y',  
+  )  
+
+def build_gltf():
+  bpy.ops.export_scene.gltf(
+    filepath=output_file, 
+  )
+
+format_to_build = {
+  'fbx': build_fbx,
+  'dae': build_dae,
+  'gltf': build_gltf,
+}
+
+
+build = format_to_build[output_format]
+assert(build != None)
+build()
+
+
+
 
