db0056a082e8fd4b792702ba925b53e10ad1daa1
brad medeiros <bradmedeiros0@gmail.com>
2021-09-15 23:31:17 -0700

    fix model -> apply transform to root model, rename it to the same as the file, set into object mode

--- try-fixmodel.py
+++ try-fixmodel.py
@@ -5,8 +5,27 @@ def delete_unwanted_objects(objtypes):
   for camera in cameras:
     bpy.data.objects.remove(camera, do_unlink=True)  
 
+def getfilename():
+  filename = bpy.path.basename(bpy.context.blend_data.filepath)
+  name = '.'.join(filename.split('.')[0:-1])  # get rid of .blend
+  if len(name) <= 0:
+    raise Exception("Cannot figure out filename")
+  return name
 
 delete_unwanted_objects(['CAMERA', 'LIGHT'])
-bpy.ops.file.make_paths_relative()
 
+if len(bpy.context.scene.objects) > 1:
+  raise Exception("Cannot fix model automatically because more than one object in scene")
+
+bpy.context.scene.objects[0].name = getfilename()
+bpy.context.scene.objects[0].select_set(True)
+bpy.ops.object.mode_set(mode="OBJECT")
+
+# Modengine also no support transforms on root node, so this must be unit (and that's nicer)
+result = bpy.ops.object.transform_apply(location=True, rotation=True, scale=True)
+is_successful = 'FINISHED' in result
+if not is_successful:
+  raise Exception("Error applying root transform to model")
+
+bpy.ops.file.make_paths_relative()
 bpy.ops.wm.save_mainfile()
